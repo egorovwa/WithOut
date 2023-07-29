f807d42e6a8c2863b89fa5fce84fa57cd7331ead
vasiliy <egorovwa2@gmail.com>
2023-07-29 18:33:28 +0700

    два дня сука

--- driver-security/src/main/java/su/egorovwa/config/SequrityConfig.java
+++ driver-security/src/main/java/su/egorovwa/config/SequrityConfig.java
@@ -1,24 +1,28 @@
 package su.egorovwa.config;
 
+import jakarta.servlet.DispatcherType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.SecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatchers;
 import su.egorovwa.security.BearerTokenConverter;
+import su.egorovwa.security.CustomAuthenticationSuccessHandler;
 import su.egorovwa.security.JwtHendle;
 
 @Configuration
 @EnableWebSecurity
+
 public class SequrityConfig {
     private final UserDetailsService userDetailsService;
     @Value("${security.secret}")
@@ -32,23 +36,31 @@ public class SequrityConfig {
     public SecurityFilterChain securityWebFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .userDetailsService(userDetailsService)
                 .authorizeHttpRequests(matcherRegistry -> {
                     matcherRegistry.requestMatchers("/auth/**").permitAll()
-                            .anyRequest().authenticated();
+                            .anyRequest().authenticated()
+                    ;
                 })
-                .addFilterAt(authenticationFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class);
+
+                .addFilterAt(authenticationFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class)
+
+        ;
         return http.build();
     }
+
     @Bean
-    public AuthenticationFilter authenticationFilter(AuthenticationManager authenticationManager){
+    public AuthenticationFilter authenticationFilter(AuthenticationManager authenticationManager) {
         AuthenticationFilter filter = new AuthenticationFilter(authenticationManager,
                 new BearerTokenConverter(new JwtHendle(secret)));
         filter.setRequestMatcher(RequestMatchers.allOf(RequestMatchers.not(AntPathRequestMatcher.antMatcher("/auth/**")),
                 RequestMatchers.allOf(AntPathRequestMatcher.antMatcher("/**"))));
+        filter.setSuccessHandler(authenticationSuccessHandler());
         return filter;
     }
 
-
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new CustomAuthenticationSuccessHandler();
+    }
 }
 
